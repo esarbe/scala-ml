9e30f9ee22e8bfe5a1ddb8f3bd05b8e2a9993ba1
Raphael Bosshard (bra) <raphael.bosshard@imis.ch>
2014-12-09 21:26:27 +0100

    replace symbols for state and observation with dedicated trait

--- src/main/scala/com/example/HMM.scala
+++ src/main/scala/com/example/HMM.scala
@@ -1,20 +1,45 @@
 package com.example
 
-
 object HMM extends App {
-  val states = Set('rainy, 'sunny)
+  trait State
   
-  val observations = Set('walk, 'shop, 'clean)
+  case object Rainy extends State
+  case object Sunny extends State
   
-  val startProbabilities = Set('rainy -> 0.6, 'sunny -> 0.4)
+  trait Observation
+  case object Walk extends Observation
+  case object Shop extends Observation
+  case object Clean extends Observation
+  
+  
+  val states = Set(Rainy, Sunny)
+  
+  val observations = Set(Walk, Shop, Clean)
+  
+  val startProbabilities = Set(Rainy -> 0.6, Sunny -> 0.4)
   
   val transitionProbabilities = Map(
-    'rainy -> Map ('rainy -> 0.7, 'sunny -> 0.3),
-    'sunny -> Map ('rainy -> 0.4, 'sunny -> 0.6)
+    Rainy -> Map (Rainy -> 0.7, Sunny -> 0.3),
+    Sunny -> Map (Rainy -> 0.4, Sunny -> 0.6)
   )
   
   val emissionProbabilities = Map(
-    'rainy -> Map('walk -> 0.1, 'shop -> 0.4, 'clean -> 0.5)
-    'sunny -> Map('walk -> 0.6, 'shop -> 0.3, 'clean -> 0.1)
+    Rainy -> Map(Walk -> 0.1, Shop -> 0.4, Clean -> 0.5)
+    Sunny -> Map(Walk -> 0.6, Shop -> 0.3, Clean -> 0.1)
   )
+  
+  val steps = 200
+  
+  def nextState(current: State): State = {
+    
+  }
+  
+  def nextEmission(state: State): 
+  
+  for { 
+    step <- (0 upto steps)
+  } yield {
+    
+  } 
+  
 }
